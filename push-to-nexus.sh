b01a67317440e47d2cd18ae61e756e3628b98f50
Sachithra_Manamperi <61633493+SachPrecious@users.noreply.github.com>
2023-03-21 10:54:34 +0530

    Update push-to-nexus.sh

--- push-to-nexus.sh
+++ push-to-nexus.sh
@@ -5,13 +5,15 @@ NEXUS_URL="http://10.101.16.48:8081"
 REPO_NAME="maven-snapshots"
 USERNAME="admin"
 PASSWORD="admin"
+MAVEN_SETTINGS=.m2/setting.xml
 
 # Set path to JAR file and artifact ID
 JAR_FILE="./target/devops-integration.jar"
 ARTIFACT_ID="devops-integration"
+-s ${MAVEN_SETTINGS}
 
 # Run Maven deploy command to push JAR file to Nexus repository
-mvn deploy:deploy-file \
+mvn -s ${MAVEN_SETTINGS} deploy:deploy-file \
   -Durl=$NEXUS_URL/repository/$REPO_NAME \
   -DrepositoryId=$REPO_NAME \
   -Dfile=$JAR_FILE \
